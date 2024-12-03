c077a78b64a3662b893000616620cbf1ef16d32b
ZXjogv <zx@jogv.es>
2024-12-03 18:56:26 +0100

    reorganize jsp.h file contents (API)

--- include/jsp.h
+++ include/jsp.h
@@ -6,26 +6,33 @@
 // #define SPECTRUM_128 to use the 128K mode memory layout.  If not
 // defined, 48K layout will be used by default
 
-extern uint8_t jsp_rottbl[];
-extern uint16_t *jsp_btt[];
-extern uint16_t *jsp_drt[];
-extern uint8_t jsp_dtt[];
-
-// some utility functions
-void jsp_memzero( void *dst, uint16_t numbytes ) __smallc __z88dk_callee;
-
-//////////////////////////
-// engine functions
-//////////////////////////
+/////////////////////////////////////////
+// Engine functions
+/////////////////////////////////////////
 
 // initialize engine, set default background tile
 void jsp_init( uint8_t *default_bg_tile );
 // redraw dirty parts of screen
 void jsp_redraw( void );
+
+/////////////////////////////////////////
+// Background tile functions
+/////////////////////////////////////////
+
 // draw 8x8 tile to BTT
 void jsp_draw_background_tile( uint8_t row, uint8_t col, uint8_t *pix ) __smallc __z88dk_callee;
 // resotres default background
 void jsp_delete_background_tile( uint8_t row, uint8_t col ) __smallc __z88dk_callee;
+
+/////////////////////////////////////////
+// Internal functions and library data
+/////////////////////////////////////////
+
+extern uint8_t	jsp_rottbl[];
+extern uint16_t	*jsp_btt[];
+extern uint16_t	*jsp_drt[];
+extern uint8_t	jsp_dtt[];
+extern uint8_t	*jsp_default_bg_tile;
 
 // mark/unmark one cell for redraw
 void jsp_dtt_mark_dirty( uint8_t row, uint8_t col ) __smallc __z88dk_callee;
@@ -37,5 +44,8 @@ void jsp_draw_screen_tile( uint8_t row, uint8_t col, uint8_t *pix ) __smallc __z
 
 // draw 8x8 tile to screen with attribute
 void jsp_draw_screen_tile_attr( uint8_t row, uint8_t col, uint8_t *pix, uint8_t attr ) __smallc __z88dk_callee;
+
+// some utility functions
+void jsp_memzero( void *dst, uint16_t numbytes ) __smallc __z88dk_callee;
 
 #endif // _JSP_H
