d08e63515683491e047895a4447ad2cdca4f5a98
ZXjogv <zx@jogv.es>
2024-12-28 16:05:41 +0100

    main: fix sprite types for tests

--- main.c
+++ main.c
@@ -130,17 +130,17 @@ void test_sprite_draw( void ) {
 
 #define NUM_SPRITES 5
 
-//DEFINE_SPRITE(sprite0,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
-//DEFINE_SPRITE(sprite1,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
+DEFINE_SPRITE(sprite0,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
+DEFINE_SPRITE(sprite1,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
 //DEFINE_SPRITE(sprite2,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
-DEFINE_SPRITE(sprite3,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
-DEFINE_SPRITE(sprite4,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
+//DEFINE_SPRITE(sprite3,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
+//DEFINE_SPRITE(sprite4,2,2,test_sprite_mask2_pixels,0,0,JSP_TYPE_MASK2);
 
-DEFINE_SPRITE(sprite0,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
-DEFINE_SPRITE(sprite1,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
+//DEFINE_SPRITE(sprite0,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
+//DEFINE_SPRITE(sprite1,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
 DEFINE_SPRITE(sprite2,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
-//DEFINE_SPRITE(sprite3,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
-//DEFINE_SPRITE(sprite4,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
+DEFINE_SPRITE(sprite3,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
+DEFINE_SPRITE(sprite4,2,2,test_sprite_load1_pixels,0,0,JSP_TYPE_LOAD1);
 
 struct { 
     uint8_t x,y;
